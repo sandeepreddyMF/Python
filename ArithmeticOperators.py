fccde93689a4b99ee040fad76ddc4a0d42530175
Sandeep Reddy <123544737+sandeepreddyMF@users.noreply.github.com>
2023-11-03 11:13:44 +0000

    arithmetic

---
+++ ArithmeticOperators.py
@@ -0,0 +1,23 @@
+number=1+2*3/4.0
+print(number)
+number=(1+2)*3/4.0
+print(number)
+number=((1+2)*3)/4.0
+print(number)
+number=1+(2*3)/4.0
+print(number)
+remainder=11%3
+print(remainder)
+square=7**2
+cube=2**3
+print(square)
+print(cube)
+helloworld='hello'+' '+'world'
+print(helloworld)
+manyhellos='hello' * 10
+print(manyhellos)
+even=[2,4,6,8]
+odd=[1,3,5,7]
+all_nums=odd+even
+print(all_nums)
+print([1,2,3]*3)
