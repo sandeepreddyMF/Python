d6c3d138a56b99ddd99adef397ae5acc84cb48f0
Sandeep Reddy <123544737+sandeepreddyMF@users.noreply.github.com>
2023-11-03 10:56:11 +0000

    Lists

---
+++ Lists.py
@@ -0,0 +1,13 @@
+mylist=[]
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+for x in mylist:
+    print(x)
+
+mylist=[1,2,3,4,5]
+print(mylist[0],mylist[4])
