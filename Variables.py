89aa9cb9b7c8fae884e2dd986febba4c11b0fe55
Sandeep Reddy <123544737+sandeepreddyMF@users.noreply.github.com>
2023-11-03 10:43:49 +0000

    Variables

---
+++ Variables.py
@@ -0,0 +1,21 @@
+myint =7
+print (myint)
+myfloat=7.0
+print(myfloat)
+mystring='hello'
+print(mystring)
+mystring="hello"
+print(mystring)
+mystring="hello 'Sandeep'"
+print (mystring)
+one=1
+two=2
+three=one+two
+print(three)
+hello='hello'
+world='world'
+helloworld=hello + ' ' + world
+print(helloworld)
+a,b,c=1,2,3
+print(a,b,c)
+#print(one + two + hello)
